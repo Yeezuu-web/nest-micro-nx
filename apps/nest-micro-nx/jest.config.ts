d072a187cfd775df48a8988b183b53225236a2c5
Yeezuu-web <yazugaming18@gmail.com>
2024-12-16 20:17:22 +0700

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/onhlMgFDmb

---
+++ apps/nest-micro-nx/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'nest-micro-nx',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nest-micro-nx',
+};
